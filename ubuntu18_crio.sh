9f9f31968eb52bde0e5b39b94019dfc9126c8ea7
Cloud-66 <cloudoblakorain@yandex.ru>
2020-03-14 17:28:47 +0300

    add create symlink

--- ubuntu18_crio.sh
+++ ubuntu18_crio.sh
@@ -25,5 +25,10 @@ sudo apt install cri-o-1.17 -y
 # dependency  package  conmon doesn't have binary
 # because of that you need to install download and install conmon package
 # https://launchpad.net/~projectatomic/+archive/ubuntu/ppa/+packages
+# it is already fixed
+
+# for 14/03/2020  fix this issue on testing repo (crio don't find runc)
+# create synlink
+ln -s /usr/lib/cri-o-runc/sbin/runc /usr/bin/runc
 
 sudo systemctl daemon-reload && sudo systemctl start crio
