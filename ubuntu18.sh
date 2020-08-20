ca1a17a43082f5ab97a0fbdac559d7bdc1b8b7eb
Cloud-66 <cloudoblakorain@yandex.ru>
2020-08-20 18:40:43 +0300

    fix

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -4,13 +4,13 @@
 ## Install Docker CE
 ## Set up the repository:
 ## Install packages to allow apt to use a repository over HTTPS
-sudo apt update -y && sudo  apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt update -y && sudo  apt install apt-transport-https ca-certificates curl software-properties-common gnupg2 -y
 
 ### Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 ### Add Docker apt repository.
-add-apt-repository \
+sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
