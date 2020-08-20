ca607e2b8bb8742ce20131f27ce1101c33394bbc
Cloud-66 <cloudoblakorain@yandex.ru>
2020-08-20 15:39:59 +0300

    add -y to install command

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -4,7 +4,7 @@
 ## Install Docker CE
 ## Set up the repository:
 ## Install packages to allow apt to use a repository over HTTPS
-sudo apt update && sudo  apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt update -y && sudo  apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 ### Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
