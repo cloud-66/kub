5e35b679563945c1f107356100886c5fefaa111a
Vadim <41143306+cloud-66@users.noreply.github.com>
2020-12-18 09:33:15 +0300

    Update ubuntu18.sh

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -60,5 +60,5 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y kubelet-1.17.* kubeadm-1.17.* kubectl-1.17.*
+sudo apt-get install -y kubelet=1.17.* kubeadm=1.17.* kubectl=1.17.*
 sudo apt-mark hold kubelet kubeadm kubectl
