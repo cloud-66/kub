047bdadca5a7b646275b527b930c649e58d24deb
Vadim <41143306+cloud-66@users.noreply.github.com>
2021-01-21 21:17:59 +0300

    Update ubuntu18.sh

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -4,50 +4,35 @@
 ## Install Docker CE
 ## Set up the repository:
 ## Install packages to allow apt to use a repository over HTTPS
-sudo apt update -y && sudo  apt install apt-transport-https ca-certificates curl software-properties-common gnupg2 -y
-
-### Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-### Add Docker apt repository.
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-
-## Install Docker CE.
-sudo apt update -y && sudo apt install docker-ce -y
-
-
-# Setup daemon.
-sudo bash -c 'cat << EOF > /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOF'
-
-
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-# Restart docker.
-sudo systemctl set-property porter.service MemoryAccounting=yes CPUAccounting=yes
-sudo systemctl set-property containerd.service MemoryAccounting=yes CPUAccounting=yes
-sudo systemctl daemon-reload && sudo systemctl enable porter && sudo systemctl restart porter
-
-sudo usermod -aG docker admin
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
 EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# (Install containerd)
+sudo apt-get update && sudo apt-get install -y containerd
+
+# Configure containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# Restart containerd
+sudo systemctl set-property containerd.service MemoryAccounting=yes CPUAccounting=yes
+sudo systemctl daemon-reload && sudo systemctl enable containerd && sudo systemctl restart containerd
+
 sudo sysctl --system
 
 # Disable swap
